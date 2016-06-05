5fa253755ddbaa447ad3fcf8ec98daaf58414994
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-05 02:57:23 -0700

    add random single 'whack-a-mole' sequence

--- sequences/randomsingle/sequence.js
+++ sequences/randomsingle/sequence.js
@@ -1,18 +1,36 @@
-// This is a right to left single block travelling sequence
+// This shows a random single block until it has occupied each space once
 
 var blockAmt = $('.sequenceblock').length;
-var blockCounter = 0;
+var iterationCounter = 0;
+
+var blockArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+var randomBlockIndex;
+var randomBlock;
+var lastBlockArray = [];
+var lastBlock;
+var blocksLeft = 16;
+
+function getRandomBlock() {
+    randomBlockIndex = Math.floor(Math.random() * blocksLeft);
+    randomBlock = blockArray[randomBlockIndex];
+    blockArray.splice(randomBlockIndex, 1);
+    lastBlockArray.push(randomBlock);
+    lastBlock = lastBlockArray[(lastBlockArray.length)-2];
+    blocksLeft = blockArray.length;
+}
 
 function sequence() {
     setTimeout(function () {
-        $('.sequenceblock:eq('+(-blockCounter - 1)+')').css('background-color', singleColor);
-        $('.sequenceblock:eq('+(-blockCounter)+')').css('background-color', defaultColor);//cleans up the previous sequence block
-        blockCounter++;
-        console.log(blockCounter);
-        if (blockCounter < blockAmt) {
+        getRandomBlock();
+        $('.sequenceblock:eq('+randomBlock+')').css('background-color', singleColor);
+        $('.sequenceblock:eq('+lastBlock+')').css('background-color', defaultColor);
+        console.log('tic');
+        if (blocksLeft !== 0) {
           sequence();
         } else {
-          blockCounter = 0;
+          blockArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+          lastBlockArray = [];
+          blocksLeft = 16;
           setTimeout(function() {
             $('.sequenceblock').css('background-color', defaultColor);
           }, 300);
