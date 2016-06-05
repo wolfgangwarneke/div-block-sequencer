057746ce40dca5982c2985146c053936819a6bf9
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-05 01:01:26 -0700

    fixed 'right to left' on two index links' text

---
+++ sequences/Snake/sequence.js
@@ -0,0 +1,34 @@
+// This is a left to right fill sequence
+
+var blockAmt = $('.sequenceblock').length;
+var blockCounter = 0;
+
+function sequence() {
+    setTimeout(function () {
+        $('.sequenceblock:eq('+blockCounter+')').css('background-color', '#ddf');
+        blockCounter++;
+        console.log(blockCounter);
+        if (blockCounter < blockAmt) {
+          sequence();
+        } else {
+          blockCounter = 0;
+          setTimeout(function() {
+            $('.sequenceblock').css('background-color', 'rgba(256, 0, 0, .2)');
+          }, 300);
+        }
+    }, 300);
+}
+
+// function manytimes(howmanytimes) {
+//   for (i = 0; i < howmanytimes; i++) {
+//     var timing = 300;
+//     setTimeout(sequence, timing);
+//     timing += 300 * 16;
+// }
+// }
+
+$(document).ready(function() {
+  // var thismany = parseInt(prompt('How many times would you like to run this sequence?'));
+  // $('.container').click(manytimes(thismany));
+  $('.container').click(sequence);
+});
