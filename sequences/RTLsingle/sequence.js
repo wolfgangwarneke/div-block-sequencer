86c0759817428a308c99f669439449763ba85fcc
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-05 00:41:52 -0700

    create right to left single block sequence

--- sequences/RTLsingle/sequence.js
+++ sequences/RTLsingle/sequence.js
@@ -1,13 +1,12 @@
-// This is a left to right "single walking through" sequence
-// It was built off of LTRfill and adds a feature to "clean up its track"
+// This is a right to left single block travelling sequence
 
 var blockAmt = $('.sequenceblock').length;
 var blockCounter = 0;
 
 function sequence() {
     setTimeout(function () {
-        $('.sequenceblock:eq('+blockCounter+')').css('background-color', '#ddf');
-        $('.sequenceblock:eq('+(blockCounter-1)+')').css('background-color', 'rgba(256, 0, 0, .2)');//cleans up the previous sequence block
+        $('.sequenceblock:eq('+(-blockCounter - 1)+')').css('background-color', singleColor);
+        $('.sequenceblock:eq('+(-blockCounter)+')').css('background-color', defaultColor);//cleans up the previous sequence block
         blockCounter++;
         console.log(blockCounter);
         if (blockCounter < blockAmt) {
@@ -15,7 +14,7 @@ function sequence() {
         } else {
           blockCounter = 0;
           setTimeout(function() {
-            $('.sequenceblock').css('background-color', 'rgba(256, 0, 0, .2)');
+            $('.sequenceblock').css('background-color', defaultColor);
           }, 300);
         }
     }, 300);
