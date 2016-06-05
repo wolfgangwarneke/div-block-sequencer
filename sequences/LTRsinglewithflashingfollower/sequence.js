f7832bffa9d08f96687a4419728c3dd3092063e3
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-06-04 21:59:09 -0700

    add new files and logic for sequence of a single LTR block with follower and flashing first block

---
+++ sequences/LTRsinglewithflashingfollower/sequence.js
@@ -0,0 +1,43 @@
+// This is a left to right single sequence with a follower block
+// It was built off of LTRfillsingle and adds a "follwer" block of a different color
+
+var blockAmt = $('.sequenceblock').length;
+var blockCounter = 0;
+
+function resetColors() {
+  setTimeout(function() {
+    $('.sequenceblock').css('background-color', 'rgba(256, 0, 0, .2)');
+    $('#flasher').toggleClass('hidden');//rest flasher at end
+  }, 300);
+}
+
+function sequence() {
+    setTimeout(function () {
+        if ( (blockCounter % 2 === 0) && (blockCounter > 1) ) {
+          $('#flasher').toggleClass('hidden');
+        }
+        $('.sequenceblock:eq('+blockCounter+')').css('background-color', '#ddf');
+        if ( !(blockCounter === 0) ) {
+          $('.sequenceblock:eq('+(blockCounter-1)+')').css('background-color', '#ff6');
+        }
+        $('.sequenceblock:eq('+(blockCounter-2)+')').css('background-color', 'rgba(256, 0, 0, .2)');//cleans sequence block before follower
+        blockCounter++;
+        console.log(blockCounter);
+        if (blockCounter < blockAmt) {
+          sequence();
+        } else {
+          blockCounter = 0;
+          setTimeout(function() {
+            $('.sequenceblock:eq('+(blockCounter-1)+')').css('background-color', '#ff6');
+            $('.sequenceblock:eq('+(blockCounter-2)+')').css('background-color', 'rgba(256, 0, 0, .2)');//cleans leftover follower
+            resetColors();
+          }, 300);
+        }
+    }, 300);
+}
+
+
+
+$(document).ready(function() {
+  $('.container').click(sequence);
+});
